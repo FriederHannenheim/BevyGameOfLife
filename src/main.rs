65a9649505f86b89b7c0b9af65b518a3bdf465d2
Frieder Hannenheim <frieder12.fml@protonmail.com>
2022-06-23 08:18:13 +0200

    Simulation speed control and grid clear

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ mod grid;
 
 pub const GRID_HEIGHT: usize = 50;
 pub const GRID_WIDTH: usize = 50;
-const SPEED: f64 = 0.5;
 
 const NEIGHBORS: [[i32;2];8] = [
     [-1,-1],[0,-1],[1,-1],
@@ -19,7 +18,8 @@ const NEIGHBORS: [[i32;2];8] = [
 
 fn main() {
     App::new()
-        .insert_resource(StateGrid([[false; GRID_HEIGHT]; GRID_WIDTH]))
+        .init_resource::<StateGrid>()
+        .insert_resource(Speed(0.1))
         .insert_resource(EntityGrid(vec![]))
         .insert_resource(Paused(true))
         .insert_resource(LastMouseCell(-1,-1))
@@ -36,6 +36,7 @@ fn main() {
                 .with_system(update_cells),
         )
         .add_system(spawn_cells_with_mouse)
+        .add_system(handle_keyboard_input)
         .add_system_to_stage(CoreStage::PostUpdate, update_cell_sprites)
         .add_plugin(GridPlugin)
         .add_plugins(DefaultPlugins)
@@ -43,8 +44,15 @@ fn main() {
 }
 
 
-
 struct StateGrid([[bool; GRID_HEIGHT]; GRID_WIDTH]);
+
+impl Default for StateGrid {
+    fn default() -> Self{
+        Self([[false; GRID_HEIGHT]; GRID_WIDTH])
+    }
+}
+
+struct Speed(f32);
 
 struct EntityGrid(Vec<Vec<Entity>>);
 
@@ -125,18 +133,14 @@ fn update_cell_sprites(
 
 
 fn should_update_run(
-    mut paused: ResMut<Paused>,
-    keyboard_input: Res<Input<KeyCode>>,
+    paused: Res<Paused>,
     time: Res<Time>,
     mut next_run_time: Local<u128>,
+    speed: Res<Speed>,
     ) -> ShouldRun {
-        if keyboard_input.just_released(KeyCode::Space) {
-            (*paused).0 = !paused.0;
-        }
-        
         let milis = time.time_since_startup().as_millis();
         if !paused.0 && milis > *next_run_time {
-            *next_run_time = milis + (SPEED * 1000.0) as u128;
+            *next_run_time = milis + (speed.0 * 1000.0) as u128;
             return ShouldRun::Yes;
         }
         ShouldRun::No
@@ -160,4 +164,25 @@ fn spawn_cells_with_mouse(
             }
         }
     }
+}
+
+fn handle_keyboard_input(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut speed: ResMut<Speed>,
+    mut paused: ResMut<Paused>,
+    mut commands: Commands,
+    ) {
+        if keyboard_input.just_released(KeyCode::Space) {
+            (*paused).0 = !paused.0;
+        }
+        if keyboard_input.just_released(KeyCode::Left) {
+            (*speed).0 = speed.0 * 1.25;
+        }
+        if keyboard_input.just_released(KeyCode::Right) {
+            (*speed).0 = speed.0 * 0.75;
+        }
+        if keyboard_input.just_released(KeyCode::C) {
+            commands.insert_resource(StateGrid::default());
+            (*paused).0 = true;
+        }
 }
